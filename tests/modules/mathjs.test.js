0b8549bae863c905d8814b464b0047401985ba63
David Stensland <me@terite.com>
2020-02-03 20:19:49 -0800

    add mathjs

---
+++ tests/modules/mathjs.test.js
@@ -0,0 +1,25 @@
+test('mathjs', () => {
+  const math = require('mathjs');
+
+  // functions and constants
+  math.round(math.e, 3)            // 2.718
+  math.atan2(3, -3) / math.pi      // 0.75
+  math.log(10000, 10)              // 4
+  math.sqrt(-4)                    // 2i
+  math.derivative('x^2 + x', 'x')  // 2*x+1
+  math.pow([[-1, 2], [3, 1]], 2)
+       // [[7, 0], [0, 7]]
+
+  // expressions
+  math.evaluate('1.2 * (2 + 4.5)')     // 7.8
+  math.evaluate('12.7 cm to inch')     // 5 inch
+  math.evaluate('sin(45 deg) ^ 2')     // 0.5
+  math.evaluate('9 / 3 + 2i')          // 3 + 2i
+  math.evaluate('det([-1, 2; 3, 1])')  // -7
+
+  // chaining
+  math.chain(3)
+      .add(4)
+      .multiply(2)
+      .done() // 14
+});
